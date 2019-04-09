fb987ccec5781cc4a863fc6ab90a83ae0976bce1
GYT0313 <guyongtao@qq.com>
2019-04-09 21:30:03 +0800

    函数和闭包

---
+++ 5-Functions and closures/8.3.scala
@@ -0,0 +1,4 @@
+// default parameter
+def printTime(out: java.io.PrintStream = Console.out) = 
+  out.println("time = " + System.currentTimeMillis())
+printTime()
---
+++ 5-Functions and closures/8.4.scala
@@ -0,0 +1,6 @@
+// default parameter
+def printTime(out: java.io.PrintStream = Console.out,
+  divisor: Int = 1) = 
+  out.println("time = " + System.currentTimeMillis() / divisor)
+printTime()
+printTime(divisor = 1000)
